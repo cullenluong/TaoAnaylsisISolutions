0718ccfb2df5551a4e3422089956c9f9f4ba4797
Terence Tao <tao@math.ucla.edu>
2025-11-08 11:37:36 -0800

    1.3.3 start

---
+++ analysis/Analysis/MeasureTheory/Section_1_3_3.lean
@@ -0,0 +1,52 @@
+import Analysis.MeasureTheory.Section_1_3_2
+
+/-!
+# Introduction to Measure Theory, Section 1.3.3: Unsigned Lebesgue integrals
+
+A companion to (the introduction to) Section 1.3.3 of the book "An introduction to Measure Theory".
+
+-/
+
+/-- Definition 1.3.12 (Lower unsigned Lebesgue integral) -/
+noncomputable def LowerUnsignedLebesgueIntegral {d:ℕ} (f: EuclideanSpace' d → EReal) : EReal :=
+  sSup { R | ∃ g: EuclideanSpace' d → EReal, ∃ hg: UnsignedSimpleFunction g, ∀ x, g x ≤ f x ∧ R = hg.integral}
+
+/-- Definition 1.3.12 (Upper unsigned Lebesgue integral) -/
+noncomputable def UpperUnsignedLebesgueIntegral {d:ℕ} (f: EuclideanSpace' d → EReal) : EReal :=
+  sInf { R | ∃ g: EuclideanSpace' d → EReal, ∃ hg: UnsignedSimpleFunction g, ∀ x, g x ≥ f x ∧ R = hg.integral}
+
+theorem LowerUnsignedLebesgueIntegral.eq {d:ℕ} (f: EuclideanSpace' d → EReal) : LowerUnsignedLebesgueIntegral f =
+  sSup { R | ∃ g: EuclideanSpace' d → EReal, ∃ hg: UnsignedSimpleFunction g, (AlmostAlways (fun x ↦ g x ≤ f x)) ∧ R = hg.integral} := by sorry
+
+/-- Exercise 1.3.10(i) (Compatibility with the simple integral) -/
+theorem LowerUnsignedLebesgueIntegral.eq_simpleIntegral {d:ℕ} {f: EuclideanSpace' d → EReal} (hf: UnsignedSimpleFunction f) :
+    LowerUnsignedLebesgueIntegral f = hf.integral := by sorry
+
+/-- Exercise 1.3.10(ii) (Monotonicity) -/
+theorem LowerUnsignedLebesgueIntegral.mono {d:ℕ} {f g: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) (hg: UnsignedMeasurable g)
+    (hfg: AlmostAlways (fun x ↦ f x ≤ g x)) :
+    LowerUnsignedLebesgueIntegral f ≤ LowerUnsignedLebesgueIntegral g := by sorry
+
+/-- Exercise 1.3.10(iii) (Homogeneity) -/
+theorem LowerUnsignedLebesgueIntegral.hom {d:ℕ} {f: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) {c: ℝ} (hc: 0 ≤ c) :
+    LowerUnsignedLebesgueIntegral ((c:EReal) • f) = c * LowerUnsignedLebesgueIntegral f := by sorry
+
+/-- Exercise 1.3.10(iv) (Equivalence) -/
+theorem LowerUnsignedLebesgueIntegral.integral_eq_integral_of_aeEqual {d:ℕ} {f g: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) (hg: UnsignedMeasurable g)
+    (heq: AlmostEverywhereEqual f g) :
+    LowerUnsignedLebesgueIntegral f = LowerUnsignedLebesgueIntegral g := by sorry
+
+/-- Exercise 1.3.10(v) (Superadditivity) -/
+theorem LowerUnsignedLebesgueIntegral.superadditive {d:ℕ} {f g: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) (hg: UnsignedMeasurable g) :
+    LowerUnsignedLebesgueIntegral (f + g) ≥ LowerUnsignedLebesgueIntegral f + LowerUnsignedLebesgueIntegral g := by sorry
+
+/-- Exercise 1.3.10(vi) (Subadditivity of upper integral)-/
+theorem UpperUnsignedLebesgueIntegral.subadditive {d:ℕ} {f g: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) (hg: UnsignedMeasurable g) :
+    UpperUnsignedLebesgueIntegral (f + g) ≤ UpperUnsignedLebesgueIntegral f + UpperUnsignedLebesgueIntegral g := by sorry
+
+/-- Exercise 1.3.10(vii) (Divisibility) -/
+theorem LowerUnsignedLebesgueIntegral.eq_add {d:ℕ} {f: EuclideanSpace' d → EReal} (hf: UnsignedMeasurable f) {E: Set (EuclideanSpace' d)} (hE: MeasurableSet E) :
+    LowerUnsignedLebesgueIntegral f = LowerUnsignedLebesgueIntegral (f * Real.toEReal ∘ E.indicator') +
+      LowerUnsignedLebesgueIntegral (f * Real.toEReal ∘ Eᶜ.indicator') := by sorry
+
+
